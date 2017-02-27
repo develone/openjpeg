7df40d1f9e2ab2aa757163953c78711b0ba8d88d
develone <develone@sbcglobal.net>
2017-02-27 14:44:56 -0700

    fixing im3

--- disp_yuv.m
+++ disp_yuv.m
@@ -4,5 +4,5 @@ figure; imagesc(im1); colorbar; colormap 'gray'
 fid = fopen('c1.bin','r'); im2 = fread(fid, [256,inf], 'int32'); fclose(fid);
 figure; imagesc(im2); colorbar; colormap 'gray'
 
-fid = fopen('c2.bin','r'); im1 = fread(fid, [256,inf], 'int32'); fclose(fid);
+fid = fopen('c2.bin','r'); im3 = fread(fid, [256,inf], 'int32'); fclose(fid);
 figure; imagesc(im3); colorbar; colormap 'gray'
