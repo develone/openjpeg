08477f00dbc02fefc5043ba0d2bad69e99c2898e
vidal <vidal@vidal-MX6438.(none)>
2017-03-03 09:59:37 -0700

    progress in compiling 20 of 29 files needed for the openjp2.a

--- build_gccultibo.sh
+++ build_gccultibo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -f *.o
+rm -f *.o *.a
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c bio.c -o bio.o
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c cidx_manager.c -o cidx_manager.o
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c cio.c -o cio.c
@@ -27,3 +27,28 @@ arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-a
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c thix_manager.c -o thix_manager.o
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c thread.c -o thread.o
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c tpix_manager.c -o tpix_manager.o
+arm-none-eabi-ar rcs libopenjp2.a *.o
+arm-none-eabi-ar t libopenjp2.a > libopenjp2_obj.txt
+cp libopenjp2.a ~/jpeg-2000-test/bare-metal/dwt
+echo "The word count here should be 20"
+echo "the word count in jpeg-2000-test/bare=metal/dwt"
+echo "when ./compile.sh is executed should be 21"
+echo "Word cpunt libopenjp2_obj.txt in src/lib/openjp2"
+wc libopenjp2_obj.txt 
+#The steps below are from the file jpeg-2000-test/zip/uart_test_code/Makefile 
+#20 of the 29 files that compile with arm-none-eabi-gcc compiler
+#two of files dwt.c & opj_malloc.c are copies from 
+#jpeg-2000-test/bare=metal/dwt
+#opj_includes.h requires memory.h commented out 
+#These files when compiled create the libopenjp2.a
+#The libopenjp2.a plus the dwtlift.o created in the jpeg-2000-test/bare=metal/dwt
+#are used to create kernel7.img which runs on Ultibo
+
+#CROSS   := arm-none-eabi-
+#CC      := $(CROSS)gcc
+#OBJECTS := $(addprefix $(OBJDIR)/,$(subst .c,.o,$(SOURCES)))
+#SOURCES := bio.c dwt.c event.c function_list.c image.c invert.c j2k.c jp2.c mct.c \
+#mqc.c opj_clock.c opj_malloc.c pi.c raw.c t1_generate_luts.c t1.c t2.c tcd.c \
+#tgt.c thread.c
+#CFLAGS  :=  -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c
+
